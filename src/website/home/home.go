0e4f3cce96df9e1033d44216b1a10ba453b87cbf
pajri <aprilio.pajri@gmail.com>
2019-03-12 13:12:52 +0700

    Fix Home Handler
    -  Add code to retrieve template files

--- src/website/home/home.go
+++ src/website/home/home.go
@@ -1,18 +1,18 @@
 package home
 
 import (
-	"net/http"
-)
-
-import (
 	"html/template"
-)
-//template used in home
-var homeTemplates = template.Must(template.ParseFiles("home.html"))
+	"net/http"
 
-func LoadHome(w http.ResponseWriter) error{
+	utils "../../utils"
+)
+
+//template used in home
+var homeTemplates = template.Must(template.ParseFiles(utils.WebsiteDirectory() + "/home/home.html"))
+
+func LoadHome(w http.ResponseWriter) error {
 	err := homeTemplates.ExecuteTemplate(w, "home.html", nil)
-	if err != nil{
+	if err != nil {
 		return err
 	}
 	return nil
