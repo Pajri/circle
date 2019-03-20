43d61f49ba42901fa64165785783ba0c0b69162e
pajri <aprilio.pajri@gmail.com>
2019-03-20 08:29:33 +0700

    Login Success Message
    - Add success message on login page after registration

--- src/website/login/login.go
+++ src/website/login/login.go
@@ -1,14 +1,47 @@
 package login
 
 import (
+	"fmt"
 	"html/template"
 	"net/http"
 	utils "../../utils"
 )
 
+//TODO check if this can be separated to make it reusasble
+type Message struct {
+	Display bool
+	Text string
+	Type string
+}
+
 var loginTemplate = template.Must(template.ParseFiles(utils.WebsiteDirectory()+"/login/login.html",
 	utils.WebsiteDirectory()+"/layout/authentication.html"))
 
 func LoginHandler(w http.ResponseWriter, r *http.Request) {
-	_ = loginTemplate.ExecuteTemplate(w, "authentication.html", nil)
+	LoginMessage := Message{
+		Display : false,
+		Text : "",
+		Type : "",
+	}
+
+	param, _ := r.URL.Query()["register"]
+	if param[0] == "success" {
+		LoginMessage = createSuccessMessage("Registration successful")
+		err := loginTemplate.ExecuteTemplate(w,"authentication.html", LoginMessage)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}else{
+		_ = loginTemplate.ExecuteTemplate(w, "authentication.html", nil)
+	}
+	
+}
+
+func createSuccessMessage(message string) Message{
+	return Message{
+		Display : true,
+		Text : message,
+		Type : "success",
+	}
+
 }
