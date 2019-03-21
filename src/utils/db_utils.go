164fa667b72444c4ead7ad167d57c14b65ce6a67
pajri <aprilio.pajri@gmail.com>
2019-03-21 08:45:16 +0700

    Just Commit

--- src/utils/db_utils.go
+++ src/utils/db_utils.go
@@ -14,6 +14,7 @@ type DbUtil struct {
 	host     string
 	database string
 	context  context.Context
+	Client *mongo.Client
 }
 
 func (db DbUtil) createURI() string {
@@ -31,15 +32,20 @@ func (db DbUtil) createURI() string {
 func (db DbUtil) Connect(ctx context.Context) (*mongo.Database, error) {
 	uri := db.createURI()
 
-	client, err := mongo.NewClient(options.Client().ApplyURI(uri))
+	var err error
+	db.Client, err = mongo.NewClient(options.Client().ApplyURI(uri))
 	if err != nil {
 		return nil, fmt.Errorf("db utils: couldn't connect to mongo: %v", err)
 	}
 
-	err = client.Connect(ctx)
+	err = db.Client.Connect(ctx)
 	if err != nil {
 		return nil, fmt.Errorf("db utils: mongo client couldn't connect with background context: %v", err)
 	}
 
-	return client.Database(db.database), nil
+	return db.Client.Database(db.database), nil
+}
+
+func (db DbUtil) Disconnect(){
+	db.Client.Disconnect(db.context)
 }
--- src/website/login/login.go
+++ src/website/login/login.go
@@ -1,17 +1,26 @@
 package login
 
 import (
+	"go.mongodb.org/mongo-driver/mongo"
+	"context"
 	"html/template"
 	"net/http"
+	
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/bson"
 
 	datamodel "../../data_model"
 	utils "../../utils"
 )
 
+var ctx context.Context
+var db utils.DbUtil
+
 var loginTemplate = template.Must(template.ParseFiles(utils.WebsiteDirectory()+"/login/login.html",
 	utils.WebsiteDirectory()+"/layout/authentication.html"))
 
 func LoginHandler(w http.ResponseWriter, r *http.Request) {
+	initHandler()
 
 	if r.Method == "POST" {
 		usrLogin := populateUsernamePassword(r)
@@ -19,10 +28,20 @@ func LoginHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	_ = loginTemplate.ExecuteTemplate(w, "authentication.html", nil)
 
+	db.Disconnect()
 }
 
-func authenticate(usrLogin datamodel.User) bool, error {
+func initHandler(){
+	ctx = context.TODO()
+	db := new(utils.DbUtil)
+	db.Connect(ctx)
+}
 
+func authenticate(usrLogin datamodel.User) (bool, error) {
+	user := new(datamodel.User)
+	findOptions := options.Find()
+	findOptions = findOptions.SetLimit(1)
+	userFound, err := db.Collection(user.CollName()).Find(ctx, bson.D{{user.UsernameColl(), username}}, findOptions)
 	return false, nil
 }
 
