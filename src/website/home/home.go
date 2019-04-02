dfc1488f316b6626ea171aabbe2ef683b50acf6b
pajri <aprilio.pajri@gmail.com>
2019-04-02 22:09:51 +0700

    Pagination
    - Add pagination

--- src/website/home/home.go
+++ src/website/home/home.go
@@ -1,13 +1,16 @@
 package home
 
 import (
-	"strings"
-	"go.mongodb.org/mongo-driver/mongo/options"
-	"go.mongodb.org/mongo-driver/bson/primitive"
-	"fmt"
 	"context"
+	"fmt"
 	"html/template"
+	"math"
 	"net/http"
+	"strconv"
+	"strings"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo/options"
 
 	"go.mongodb.org/mongo-driver/bson"
 
@@ -25,42 +28,77 @@ var homeTemplates = template.Must(template.ParseFiles(utils.WebsiteDirectory()+"
 var db *mongo.Database
 var ctx context.Context
 
+type HomeViewModel struct {
+	Questions   []*datamodel.Question
+	CurrentPage int
+	PageIndex   []int
+}
+
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
-	splitString := strings.Split(r.URL.Path,"/")
-	fmt.Println("split string : ",splitString)
-	if len(splitString) > 2 {
-		fmt.Println("page number :",splitString[3])
-	}
-	
 	var err error
 	var session *sessions.Session
+	homeViewModel := HomeViewModel{
+		Questions:   nil,
+		CurrentPage: 1,
+		PageIndex:   nil,
+	}
 
+	//process session
 	session, err = utils.GetSession(r, utils.SESSION_AUTH)
 	if err != nil {
 		utils.InternalServerErrorHandler(w, r, err, "home : an error occured when executing templates")
 		return
 	}
 
+	//init home
 	err = InitHome()
 	if err != nil {
 		utils.InternalServerErrorHandler(w, r, err, "home : an error occured when executing templates")
 		return
 	}
 
+	//check is logged in
 	isAuth := utils.IsLoggedInSession(session)
 	if !isAuth {
 		utils.ForbiddenHandler(w, r)
 		return
 	}
 
-	var questions []*datamodel.Question
-	questions, err = ListQuestion()
+	//get page for pagination
+	splitUrl := strings.Split(r.URL.Path, "/")
+	if len(splitUrl) == 4 { // the url like /home/page/3, the first element is empty string : [,home,page,3]
+		if splitUrl[2] == "page" {
+			var cur int64
+			cur, err = strconv.ParseInt(splitUrl[3], 10, 64)
+			if err != nil {
+				utils.InternalServerErrorHandler(w, r, err, "home : an error occured when casting page num")
+				return
+			}
+			homeViewModel.CurrentPage = int(cur)
+		}
+	}
+
+	count := int64(0)
+	count, err = db.Collection(datamodel.CollQuestion).CountDocuments(ctx, bson.D{}, nil)
+	fmt.Println("count : ", count)
+	if err != nil {
+		utils.InternalServerErrorHandler(w, r, err, "home : an error occured when counting documents")
+		return
+	}
+	pagesCount := int(math.Ceil(float64(count) / 5))
+	fmt.Println("pages count : ", pagesCount)
+	homeViewModel.PageIndex = make([]int, pagesCount)
+	for i := 0; i < int(pagesCount); i++ {
+		homeViewModel.PageIndex[i] = i + 1
+	}
+
+	homeViewModel.Questions, err = ListQuestion(homeViewModel.CurrentPage)
 	if err != nil {
 		utils.InternalServerErrorHandler(w, r, err, "home : an error occured when load list question")
 		return
 	}
 
-	err = homeTemplates.ExecuteTemplate(w, "main.html", questions)
+	err = homeTemplates.ExecuteTemplate(w, "main.html", homeViewModel)
 	if err != nil {
 		utils.InternalServerErrorHandler(w, r, err, "home : an error occured when executing templates")
 		return
@@ -75,12 +113,18 @@ func InitHome() error {
 	return err
 }
 
-func ListQuestion() ([]*datamodel.Question, error) {
+func ListQuestion(page int) ([]*datamodel.Question, error) {
+	if page > 0 {
+		page = page - 1
+	}
+
 	opt := options.Find()
-	opt.SetLimit(5)
+	limit := 5
+	opt.SetLimit(int64(limit))
 	opt.SetSort(bson.D{{datamodel.FieldQuestionCreatedDate, -1}})
-	
-	c, err := db.Collection(datamodel.CollQuestion).Find(ctx, bson.D{},opt)
+	opt.SetSkip(int64(page * limit))
+
+	c, err := db.Collection(datamodel.CollQuestion).Find(ctx, bson.D{}, opt)
 	if err != nil {
 		return nil, err
 	}
@@ -99,7 +143,7 @@ func ListQuestion() ([]*datamodel.Question, error) {
 		m := doc.Map()
 
 		question.Title = m[question.TitleColl()].(string)
-		question.Description = m[question.DescriptionColl()].(string)[:100]+"..."
+		question.Description = m[question.DescriptionColl()].(string)[:100] + "..."
 		question.Vote = int(m[question.VoteColl()].(float64))
 		question.IsSolved = m[question.IsSolvedColl()].(bool)
 		question.Username = m[question.UsernameColl()].(string)
